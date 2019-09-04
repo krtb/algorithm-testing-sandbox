068ce616f44315ce7c4056aeebe765131f16c56b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-04 17:24:21 -0400

    final solution

--- HackRankChallenges/SockMerchant/SockMerchant.js
+++ HackRankChallenges/SockMerchant/SockMerchant.js
@@ -1,6 +1,6 @@
-let sampleArray = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+let arr = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
-function sockMerchant(n, ar) {
+function sockMerchant(n, arr) {
     //makes sure elements sorted in ASCENDING order
     let holdSortedSocks = arr.sort((a, b) => a - b);
     //variable would hold final count of amount of pairs, starts at 0
@@ -8,8 +8,11 @@ function sockMerchant(n, ar) {
 
     //since comparing items side by side, make a full stop at last index
     for( let i = 0; i < n - 1; ++1 ){
-        if (holdSortedSocks[i] === holdSortedSocks[i] + 1 ){
+        if (holdSortedSocks[i] === holdSortedSocks[i + 1]){
             pairs++ //increment the pairs value if we find a match
+            i += 1
         }
     }
+
+    return pairs
 }
