53736410203b88f47a9b2d68968d21e485b235f0
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-04 17:28:06 -0400

    add notes and initial func setup

---
+++ HackRankChallenges/CountingValleys/countingValleys.js
@@ -0,0 +1,6 @@
+function countingValleys(n, s) {
+    //n = number of steps in Gary's hike
+    // s = string of n chracters that describe Gary's path
+
+
+}
---
+++ HackRankChallenges/CountingValleys/countingValleys.md
@@ -0,0 +1,3 @@
+# Notes
+* Need to keep track of valleys, will have a count of this at the end.
+  * "find and print number ofvalleys Gary walked through"
