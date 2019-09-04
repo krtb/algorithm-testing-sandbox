50de1dd0bc52c2d87de4f3da4d18dda1666f940c
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-04 16:31:35 -0400

    add solution with for-of loop

--- AlgoCasts/exercises/chunk/chunkNotes.md
+++ AlgoCasts/exercises/chunk/chunkNotes.md
@@ -1,7 +1,7 @@
 # Solution Steps
 
 * Create empty array tohold chunks, called `chunked`
-* For each element  in the `unchunked` array
+* For each element in the `unchunked` array
   * retrieve the last element
     * would INTITIALLY get undefined, since new array is first empty
   * `IF` last element does not exist, or if it's length is equal to chunk size
--- AlgoCasts/exercises/chunk/index.js
+++ AlgoCasts/exercises/chunk/index.js
@@ -8,6 +8,24 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    //1) create new array
+    const chunked = [];
+    //2) iterate through original array of elements
+    for (let element of array ) {
+    //3) get last elememt inside our chunked array
+        const last =  chunked[chunked.length - 1] //get last element
+        console.log(last);
+        
+        if (!last || last.length === size){
+            //push new chunk into chunked with current element
+            chunked.push([element]) // add a new chunk, and put element inside, 2 things in one step
+        } else {
+            last.push(element)
+        }
+    }
+
+    return chunked;
+}
 
 module.exports = chunk;
