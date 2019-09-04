93a9ee1e3d81a22d45eb1fdfc853e6a9a8ec55b3
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-04 16:56:17 -0400

    add array of numnbers to sort

--- HackRankChallenges/SockMerchant/SockMerchant.js
+++ HackRankChallenges/SockMerchant/SockMerchant.js
@@ -1,3 +1,5 @@
+let sampleArray = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+
 function sockMerchant(n, ar) {
     //makes sure elements sorted in ASCENDING order
     let holdSortedSocks = arr.sort((a, b) => a - b);
