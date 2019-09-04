ee64d0d489795b4ec28a834f44e91710bc2b50ff
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-04 16:54:04 -0400

    add sort func, with comparison. set counter of pairs at 0

---
+++ HackRankChallenges/SockMerchant/SockMerchant.js
@@ -0,0 +1,6 @@
+function sockMerchant(n, ar) {
+    //makes sure elements sorted in ASCENDING order
+    let holdSortedSocks = arr.sort((a, b) => a - b);
+    //variable would hold final count of amount of pairs, starts at 0
+    let pairs = 0;
+}
