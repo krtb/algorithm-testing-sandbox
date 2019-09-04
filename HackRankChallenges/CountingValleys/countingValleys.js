9fc762d26eaf341db09b766c836de1ef9dcfc009
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-04 17:33:09 -0400

    add variable to hold integers tracking Gary's travel

--- HackRankChallenges/CountingValleys/countingValleys.js
+++ HackRankChallenges/CountingValleys/countingValleys.js
@@ -1,6 +1,7 @@
 function countingValleys(n, s) {
     //n = number of steps in Gary's hike
     // s = string of n chracters that describe Gary's path
-
+    let numberOfValleys = 0;
+    let currentLevel = 0; // start at 0 for regulat path, -1 when in valley, +1 when climbing mountain
 
 }
--- HackRankChallenges/CountingValleys/countingValleys.md
+++ HackRankChallenges/CountingValleys/countingValleys.md
@@ -1,3 +1,5 @@
 # Notes
 * Need to keep track of valleys, will have a count of this at the end.
   * "find and print number ofvalleys Gary walked through"
+* Need to track how far above or below sea level gary is
+  * create a variable to hold number for this
