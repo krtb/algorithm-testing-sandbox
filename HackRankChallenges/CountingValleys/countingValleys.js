9e51ccef96c60108484d9ad6cdf0d6d4794a3e58
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-06 10:29:14 -0400

    add stack exampled

--- HackRankChallenges/CountingValleys/countingValleys.js
+++ HackRankChallenges/CountingValleys/countingValleys.js
@@ -3,5 +3,14 @@ function countingValleys(n, s) {
     // s = string of n chracters that describe Gary's path
     let numberOfValleys = 0;
     let currentLevel = 0; // start at 0 for regulat path, -1 when in valley, +1 when climbing mountain
-
+    let d;
+    
+    for(d in s){
+        if(d === 'U'){
+            currentLevel += 1
+        } else {
+            currentLevel -= 1
+        }
+    }
+    return numberOfValleys
 }
---
+++ Youtube/freeCodeCamp1/stacks.js
@@ -0,0 +1,5 @@
+/* STACKS */
+
+// functions: push, pop, peep, length
+
+var letters = []; // Here is our STACK
