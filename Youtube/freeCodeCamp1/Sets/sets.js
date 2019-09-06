f2065a98a3143f8a15308a3b461468fff9509381
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-06 12:21:29 -0400

    setup func, add empty array to hold set

---
+++ Youtube/freeCodeCamp1/Sets/sets.js
@@ -0,0 +1,10 @@
+/*SETS*/
+// USE CASE: check for presence of an item
+
+// like array, without duplicate items  
+// values not in order
+// ES6 has built in SET object, BUT missing functions
+
+function mySet(){
+    let collection = []; // collection will hold the set
+}
