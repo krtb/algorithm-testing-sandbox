2c6b905543104185039c20e4ea22357bfffaedd8
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-06 11:45:15 -0400

    add pop function

--- Youtube/freeCodeCamp1/Stacks/stackClass.js
+++ Youtube/freeCodeCamp1/Stacks/stackClass.js
@@ -5,7 +5,20 @@ let Stack = function(){
 
     //Adds a value onto the END of the stack
     this.push = function(value){
-        this.storage[this.count] = value
-        this.count++;  
+        this.storage[this.count] = value; // going to put the value at the INDEX of the count, starts at 0
+        this.count++;  // inctease the count
+    }
+
+    //REMOVE and return value at the end of the stack
+    this.pop = function() {
+        // if nothing in stack, return undefined
+        if(this.count === 0){
+            return undefined; //breaks from code
+        }
+
+        this.count--; // want to decrement count, because popping off  
+        let result = this.storage[this.count];
+        delete this.storage[this.count]; // delete: removes prop from object
+        return result; // result that we stores before deleting
     }
 }
