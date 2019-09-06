8279c8e1d96eea8cc7d2b91ad68398a4f32f1cfb
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-06 10:40:44 -0400

    cerate other forLoop to place words in reverse

--- Youtube/freeCodeCamp1/stacks.js
+++ Youtube/freeCodeCamp1/stacks.js
@@ -12,7 +12,13 @@ var word = "racecar";
 
 var reversedWord = "";
 
+//TODO: 1) place your letters into the stack
 //put letters of word into stack
 for(let i = 0; i < word.length; i++){
     letters.push(word[i]); //push each letter of our [word] at index [i], go through every letter
+}
+
+//TODO: 2) pop off the stack in reverse order
+for(let i = 0; i < word.length; i++){
+    
 }
