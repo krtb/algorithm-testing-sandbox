083e238cf5d27427af4641fe90ec9d9f5fba9b5a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-06 10:38:31 -0400

    add for loop to push each letter of our word to the stack

--- Youtube/freeCodeCamp1/stacks.js
+++ Youtube/freeCodeCamp1/stacks.js
@@ -1,6 +1,10 @@
 /* STACKS */
 
-// functions: push, pop, peep, length
+// functions: 
+// push = adds 1 or more items to the end of the array
+// pop =  removes 1 or more items from the end of the array
+// peep = 
+// length = returns length of string 
 
 var letters = []; // Here is our STACK
 
@@ -10,5 +14,5 @@ var reversedWord = "";
 
 //put letters of word into stack
 for(let i = 0; i < word.length; i++){
-    
+    letters.push(word[i]); //push each letter of our [word] at index [i], go through every letter
 }
