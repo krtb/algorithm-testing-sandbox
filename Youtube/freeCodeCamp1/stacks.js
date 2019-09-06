6137402d69acd9b111c805f0f47bfd1f171089bd
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-06 10:53:28 -0400

    use pop method to add lettrs to string in reverse

--- Youtube/freeCodeCamp1/stacks.js
+++ Youtube/freeCodeCamp1/stacks.js
@@ -1,8 +1,8 @@
 /* STACKS */
 
 // functions: 
-// push = adds 1 or more items to the end of the array
-// pop =  removes 1 or more items from the end of the array
+// push = adds 1 or more items to the END of the array
+// pop =  removes 1 or more items from the END of the array
 // peep = 
 // length = returns length of string 
 
@@ -16,9 +16,13 @@ var reversedWord = "";
 //put letters of word into stack
 for(let i = 0; i < word.length; i++){
     letters.push(word[i]); //push each letter of our [word] at index [i], go through every letter
+    console.log(letters)
 }
 
 //TODO: 2) pop off the stack in reverse order
 for(let i = 0; i < word.length; i++){
-    
+    reversedWord += letters.pop(); //add 1 letter at the time to the new string
+    // where put in order
+    // will be popped off in reverse order 
+    console.log(reversedWord)
 }
