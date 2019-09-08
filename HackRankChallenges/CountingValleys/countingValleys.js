8e276868f56b15466122bf764425da0f87d8fa12
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-08 19:45:11 -0400

    add checks return final count

--- HackRankChallenges/CountingValleys/countingValleys.js
+++ HackRankChallenges/CountingValleys/countingValleys.js
@@ -3,11 +3,19 @@
 
 function countingValleys(n, s) {
   //create two values, one for altitude and one to hold count of number of valleys
- let h = 0;
- let count = 0;
+    let h = 0;
+    let count = 0;
 
- for( let i = 0; i < n; i++ ){
-     if()
- }
-
+    for( let i = 0; i < n; i++ ){
+        //returns character at specified index
+        if(s.charAt(i) === "D" && h == 0){
+            count++;
+            h--;
+        } else if (s.charsAt(i) === "D" && h !== 0){
+            h--;
+        } else if (s.charAt(i) === "U") {
+            h++;
+        }
+    }
+    return count; 
 }
