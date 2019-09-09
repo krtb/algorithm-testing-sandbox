246e243757049b217cdd63228ccdd1c522028f61
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-09 09:18:42 -0400

    setup variables to create object

--- HackRankChallenges/RepeatedString/notes.md
+++ HackRankChallenges/RepeatedString/notes.md
@@ -6,6 +6,6 @@
 
 # Function description
 * should return an integer representing the number of occurrences of a 
-* in the prefix of length 
+* in the prefix of length (n)
 * in the infinitely repeating string
 
---
+++ HackRankChallenges/RepeatedString/repeatString.js
@@ -0,0 +1,10 @@
+let s = aba;
+let n = 10;
+
+function repeatedString(s, n) {
+    const charMap = {}; // object to hold key(letters from s) value(number of times element found) pairs
+    let count = 0; // counter for mx number
+    let newString = ''; // 
+    
+
+}
