9e6179d18d44a2e9e715a187e892f38f1c99b5ff
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-09 10:42:41 -0400

    add repeatString template

--- HackRankChallenges/RepeatedString/repeatString.js
+++ HackRankChallenges/RepeatedString/repeatString.js
@@ -1,10 +1,8 @@
-let s = aba;
+let s = 'aba';
 let n = 10;
 
 function repeatedString(s, n) {
-    const charMap = {}; // object to hold key(letters from s) value(number of times element found) pairs
-    let count = 0; // counter for mx number
-    let newString = ''; // 
     
-
 }
+
+repeatedString(s, n)
