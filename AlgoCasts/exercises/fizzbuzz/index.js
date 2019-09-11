be1fe3e305fa74b0929799a04af418722fb77ce9
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-11 15:36:57 -0400

    solve using for loop

--- AlgoCasts/exercises/fizzbuzz/index.js
+++ AlgoCasts/exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+//TODO: NOTES
+// - use modulo, (%), to make sure that you're not getting a remainder
+let n = 33
+function fizzBuzz(n) {
+    //need to iterate from 1 to n, so need to use classic for loop
+    for (let i = 1; i <= n; i++ ){
+        if (i % 3 === 0 && i % 5 === 0){ // is the number a multiple of 3 and 5. Could also make it 15
+            console.log('fizzbuzz')
+        } else if (i % 3 === 0){
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzBuzz(n)
 
 module.exports = fizzBuzz;
