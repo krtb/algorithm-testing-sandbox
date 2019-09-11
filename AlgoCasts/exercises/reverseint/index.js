1751e9a149c92688fe4a5a03e919739f07f060e6
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-11 14:09:02 -0400

    uncomment previous solution

--- AlgoCasts/exercises/reverseint/index.js
+++ AlgoCasts/exercises/reverseint/index.js
@@ -17,18 +17,19 @@
 //    - converts string to number, without decimals or specical characters, also NAN if not a number
 let n = -12345
 
-// function reverseInt(n) {
-//     let reversedNumber = n.toString().split('').reverse().join('')
-//     let cleanNumber = parseInt(reversedNumber)
+//TODO: solution 1
+function reverseInt(n) {
+    let reversedNumber = n.toString().split('').reverse().join('')
+    let cleanNumber = parseInt(reversedNumber)
 
-//     if(n > 0){
-//         console.log(cleanNumber);
-//     } else {
-//         console.log(cleanNumber * -1 )
-//     }
+    if(n > 0){
+        console.log(cleanNumber);
+    } else {
+        console.log(cleanNumber * -1 )
+    }
 
-//     //console.log(cleanNumber);    
-// }
+    //console.log(cleanNumber);    
+}
 
 //TODO: cleaner solution with Math.sign(), returns positive 1 if positive and negative -1 if negative
 function reverseInt(n) {
