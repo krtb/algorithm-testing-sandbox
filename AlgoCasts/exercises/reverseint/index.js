3ea844a26807578494b6a868de9ac798f5b521ac
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-11 14:04:35 -0400

    add clearner solution, remove if/else statement

--- AlgoCasts/exercises/reverseint/index.js
+++ AlgoCasts/exercises/reverseint/index.js
@@ -17,17 +17,26 @@
 //    - converts string to number, without decimals or specical characters, also NAN if not a number
 let n = -12345
 
+// function reverseInt(n) {
+//     let reversedNumber = n.toString().split('').reverse().join('')
+//     let cleanNumber = parseInt(reversedNumber)
+
+//     if(n > 0){
+//         console.log(cleanNumber);
+//     } else {
+//         console.log(cleanNumber * -1 )
+//     }
+
+//     //console.log(cleanNumber);    
+// }
+
+//TODO: cleaner solution with Math.sign(), returns positive 1 if positive and negative -1 if negative
 function reverseInt(n) {
     let reversedNumber = n.toString().split('').reverse().join('')
     let cleanNumber = parseInt(reversedNumber)
 
-    if(n > 0){
-        console.log(cleanNumber);
-    } else {
-        console.log(cleanNumber * -1 )
-    }
-
-    //console.log(cleanNumber);    
+    //console.log(cleanNumber * Math.sign(n));
+    return cleanNumber * Math.sign(n)
 }
 
 reverseInt(n)
