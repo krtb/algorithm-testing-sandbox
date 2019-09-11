fb80a76e549d4ed186cf13261c48f98430f85f5d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-09-11 14:46:05 -0400

    add clean for loop with ternary

--- AlgoCasts/exercises/maxchar/index.js
+++ AlgoCasts/exercises/maxchar/index.js
@@ -5,31 +5,47 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+//TODO: NOTES
+// - common question forms
+//    - what is the most common character in the string?
+//    - Does string A have the same characters as string B? (ANAGRAM)
+//    - Does the repeated string have any common characters in it?
+
+let str = "asdfijerllllllllasdfjkjweweadascawdc"
+
 function maxChar(str) {
     //empty object will hold our data
     const charMap = {};
     let max = 0;
     let maxChar = '';
 
-    //FOR-OF, iterating through any iterable object. Array, string, etc.
-    for(let char of str){
-        if(!charMap[char]){
-            charMap[char] = 1
-        } else {
-            charMap[char]++
-        }
-    }
-    //console.log(charMap)
+    // for(let char of str){
+    //     if (!charMap[char]) {// check VALUE of key
+    //         charMap[char] = 1 // if none, set first key to have property of 1
+    //     } else {
+    //         charMap[char]++ // keep adding onto value of key
+    //     }
+    // }
 
-    //FOR-IN for objects
-    for(let char in charMap) {
-        if(charMap[char] > max){
-            // if element greater than this character, new max value
-            max = charMap[char]
-            maxChar = char //char is diff letters added to object
-        }
+    //CLEAN VERSION
+    for (let char of str) {
+        charMap[char] = charMap[char] + 1 || 1;
     }
-    return maxChar;
+
+    // //FOR-IN for objects
+    // for(let char in charMap) {
+    //     if(charMap[char] > max){
+    //         // if element greater than this character, new max value
+    //         max = charMap[char]
+    //         maxChar = char //char is diff letters added to object
+    //     }
+    // }
+    // return maxChar;
+
+    console.log(charMap)
+
 }
+
+maxChar(str)
 
 module.exports = maxChar;
