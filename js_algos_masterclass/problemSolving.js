d149a09e68ad3c7d5a591129f39430095aaac939
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-15 14:23:58 -0500

    create func to check if alphanumeric w/ charCode

--- js_algos_masterclass/problemSolving.js
+++ js_algos_masterclass/problemSolving.js
@@ -88,4 +88,18 @@ function charCount(str) {
     console.log(obj)
 }
 
+// ---------- REFACTOR 3 --------- //
+
+function isAlphaNumeric(char){
+    let code = char.charCodeAt(0)
+
+    if(!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)
+    ){
+        return false
+    }
+    return true
+}
+
 charCount(str)
