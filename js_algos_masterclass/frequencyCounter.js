fa031de48d35678d1981ef1c8ea32f515cb981c6
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-15 14:53:08 -0500

    add frequencyCounter to tracking

---
+++ js_algos_masterclass/frequencyCounter.js
@@ -0,0 +1,9 @@
+// Q: "Write a function called Same(), which accepts 2 arrays."
+//     Function should return true IF every value in the array,
+//     has it's corresponding value SQUARED in the 2nd array.
+//      Frequency of values must be the same
+
+// Ex: same([1,2,3], [4,1,9]) // TRUE
+//     same([1,2,3],[1,9]) // FALSE
+//     same([1,2,1], [4,4,1]) // FALSE (must be the same frequency)
+
