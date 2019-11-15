732e808a8a9334e47fec68ce39900ee10efe7fdd
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-15 14:25:45 -0500

    add inline notes on what code checks

--- js_algos_masterclass/problemSolving.js
+++ js_algos_masterclass/problemSolving.js
@@ -93,9 +93,9 @@ function charCount(str) {
 function isAlphaNumeric(char){
     let code = char.charCodeAt(0)
 
-    if(!(code > 47 && code < 58) &&
-        !(code > 64 && code < 91) &&
-        !(code > 96 && code < 123)
+    if(!(code > 47 && code < 58) && // numeric (0-9)
+        !(code > 64 && code < 91) && // upper alpha (A-Z)
+        !(code > 96 && code < 123) // lower alpha (a-z)
     ){
         return false
     }
