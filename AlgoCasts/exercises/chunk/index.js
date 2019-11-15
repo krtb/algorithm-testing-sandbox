492615fc88625f0baa85e49b79b63c4f84a5bc0a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-15 09:51:38 -0500

    place on new line

--- AlgoCasts/exercises/chunk/index.js
+++ AlgoCasts/exercises/chunk/index.js
@@ -15,15 +15,16 @@
 function chunk(array, size) {
     //1) create new array
     const chunked = [];
+    console.log(!chunked)
     //2) iterate through original array of elements
     for (let element of array ) {
     //3) get last elememt inside our chunked array
 
         //STILL AN ARRAY BECAUSE AT NO TIME ARE YOU MUTATING THE DATA TYPE
         const last =  chunked[chunked.length - 1] // LENGTH OF THE ARRAY - 1 =  you end up on the right side
-        //console.log(last);
         
-        if (!last || last.length === size){
+        //1
+        if ( ! last || last.length === size){ //OR
             //push new chunk into chunked with current element
             chunked.push([element]) // add a new chunk, and put element inside, 2 things in one step
         } else {
