cc1d64620fdaa0e08e0a1af54cb616e14bdb755d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-16 10:41:13 -0500

    add frequency counter algo pattern

--- js_algos_masterclass/frequencyCounter.js
+++ js_algos_masterclass/frequencyCounter.js
@@ -7,3 +7,36 @@
 //     same([1,2,3],[1,9]) // FALSE
 //     same([1,2,1], [4,4,1]) // FALSE (must be the same frequency, 1 should occur twice)
 
+
+function same(arr1, arr2){
+    //short-circuit
+    if(arr1.length !== arr2.length){
+        return false;
+    }
+
+    // each obj counts frequency of individual values in arrays  
+    let frequencyCounter1 = {}; 
+    let frequencyCounter2 = {};
+
+    for(let val of arr1){
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+
+    for(let val of arr2){
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1
+    }
+
+    for(let key in frequencyCounter1){
+        if(!(key ** 2 in frequencyCounter2)){
+            return false
+        }
+
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]){
+            return false
+        }
+    }
+    console.log(frequencyCounter1)
+    console.log(frequencyCounter2)
+}   
+
+same([1,2,3,2], [9,1,4,4])
