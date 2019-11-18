b7cd71cbedbfec91c01dceb88f1e4c179cd3a66c
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-18 12:44:44 -0500

    slice usage

--- Methods/slice.js
+++ Methods/slice.js
@@ -6,3 +6,7 @@ console.log(citrus)
 // DEFINITION
 // - returns the selected elements in an array
 // - as a new array object {}
+
+// USAGE: array.slice(start, end)
+// - selects the elements starting at the given *start argument*
+// -  ends at, *but does not include*, the given *end argument*
